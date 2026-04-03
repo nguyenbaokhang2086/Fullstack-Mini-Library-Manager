d87e73a191e581fb4ac44ca4609f5349ad1586f4
nguyenbaokhang2086 <nguyenbaokhang891@gmail.com>
2026-04-03 19:04:17 +0700

    feat: initialize frontend project with Vite, React, Tailwind CSS, and PWA configuration

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,36 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'; // 1. Import plugin
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // 2. Cấu hình PWA
+    VitePWA({
+      registerType: 'autoUpdate', // Tự động cập nhật khi có nội dung mới
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Fullstack Mini Library Manager',
+        short_name: 'Fullstack Mini Library Manager',
+        description: 'Fullstack Mini Library Manager',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-logo.png', // Bạn phải chuẩn bị icon này trong thư mục public
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-logo.png', // Bạn phải chuẩn bị icon này trong thư mục public
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
